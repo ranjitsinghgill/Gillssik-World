198e7bc97c52ce6e9f6274fa8c68f8032e243e38
ranjitsgill <gillzrx@gmail.com>
2022-11-20 18:01:36 +1100

    Made it better

--- README.md
+++
@@ -1,5 +0,0 @@
-## Gillssik World
-
-💜
-
-Made by Ranjit Singh Gill 👨‍💻
--- index.js
+++ index.js
@@ -1,1 +1,45 @@
- 
+const galleryContainer = document.getElementById('gallery-container')
+
+
+
+
+let isLiked = false
+let isShared = false
+
+
+document.addEventListener('click', function(e){
+    if(e.target.dataset.heart){
+        isLiked = !isLiked
+        render()
+    }
+    else if(e.target.dataset.share){
+        isShared = !isShared
+        render()
+    }
+})
+
+function render(){ 
+    
+    let heartClass = ''
+    let shareClass = ''
+    
+    if(isLiked){
+        heartClass = 'liked'
+    }
+    
+    if(isShared){
+        shareClass = 'shared'
+    }
+       
+    let imageHtml = `
+    		<div id="image-1" class="img-container">
+			<img src="dino2.jpeg" alt="Kids in front of dinosaur">
+			<div class="social-icons-container">
+				<i class="fa-solid fa-heart ${heartClass}" data-heart="image-1"></i>
+				<i class="fa-solid fa-share ${shareClass}" data-share="image-1"></i>
+			</div>
+    `
+    galleryContainer.innerHTML = imageHtml
+}
+
+render()
